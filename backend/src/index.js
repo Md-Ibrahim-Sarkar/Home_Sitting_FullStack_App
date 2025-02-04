967b1740b7fef53f4a0a33287e815ca63d1952b1
Md-Ibrahim-Sarkar <ibrahim06978@gmail.com>
2025-02-04 23:46:32 +0600

    added

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,12 +36,22 @@ app.use('/api/booking', bookedRouter)
 app.use('/api/favorite', favoriteRouter)
 
 // this 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
   app.get("*", (req, res) => {
     app.sendFile(path.join(__dirname, "../frontend/dist", "dist", "index.html"))
   })
+}
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+
 }
 
 
